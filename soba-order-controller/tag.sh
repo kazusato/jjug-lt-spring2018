9a5d34ab19d33f6196ba88d09b842f289b8188ee
Kazuhiko Sato <super80@gmail.com>
2018-05-22 21:07:18 +0900

    modified image version

--- soba-order-controller/tag.sh
+++ soba-order-controller/tag.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker tag kazusato/soba-order-controller:0.1.3 demoreg.azurecr.io/kazusato/soba-order-controller:0.1.3
+docker tag kazusato/soba-order-controller:0.1.4 demoreg.azurecr.io/kazusato/soba-order-controller:0.1.4
